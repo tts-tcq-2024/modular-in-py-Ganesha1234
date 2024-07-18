b8a7173c29932d457bd63d532008542d230c7fea
Ganesha A S <93441611+Ganesha1234@users.noreply.github.com>
2024-07-18 10:51:51 +0530

    Create manual.py

---
+++ manual.py
@@ -0,0 +1,18 @@
+from color_code import MAJOR_COLORS, MINOR_COLORS, Collect_pair_number_from_color, color_pair_to_string
+
+
+def create_reference_manual():
+    manual = []
+    for major in MAJOR_COLORS:
+        for minor in MINOR_COLORS:
+            pair_number = Collect_pair_number_from_color(major, minor)
+            manual.append(f'{pair_number:2d} - {color_pair_to_string(major, minor)}')
+    print("manual prepared")
+    return '\n'.join(manual)
+
+
+if __name__ == '__main__':
+    print("start")
+    manual = create_reference_manual()
+    print(manual)
+    print("End")
