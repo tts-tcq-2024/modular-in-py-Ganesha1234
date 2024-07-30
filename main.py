f78f8d3d1696b72afe339f8e02c61ce24b98c09c
Ganesha A S <93441611+Ganesha1234@users.noreply.github.com>
2024-07-30 15:22:19 +0530

    Update main.py

--- main.py
+++ main.py
@@ -1,15 +1,19 @@
 from tests import run_tests
-from CreateReferenceManual import create_reference_manual
 
 
-def main():
-    # Running tests
+def create_reference_manual():
+    manual = []
+    for major in MAJOR_COLORS:
+        for minor in MINOR_COLORS:
+            pair_number = Collect_pair_number_from_color(major, minor)
+            manual.append(f'{pair_number:2d} - {color_pair_to_string(major, minor)}')
+    return '\n'.join(manual)
+  
+
+
+if __name__ == '__main__':  
+     # Running tests
     run_tests()
 
     # Generating reference manual
     manual = create_reference_manual()
-    print(manual)
-
-
-if __name__ == '__main__':  
-    main()
