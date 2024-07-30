74354f57601e4dc657de9bbfe7c4c86830419f7d
Ganesha A S <93441611+Ganesha1234@users.noreply.github.com>
2024-07-30 16:04:07 +0530

    Update FindColorcode.py

--- FindColorcode.py
+++ FindColorcode.py
@@ -1,10 +1,12 @@
 MAJOR_COLORS = ['White', 'Red', 'Black', 'Yellow', 'Violet']
 MINOR_COLORS = ["Blue", "Orange", "Green", "Brown", "Slate"]
 
-def color_pair_to_string(major_color, minor_color):
+
+def format_color_pair_string(major_color, minor_color):
     return f'{major_color} {minor_color}'
 
-def Collect_color_from_pair_number(pair_number):
+
+def get_color_from_pair_number(pair_number):
     zero_based_pair_number = pair_number - 1
     major_index = zero_based_pair_number // len(MINOR_COLORS)
     if major_index >= len(MAJOR_COLORS):
@@ -14,13 +16,15 @@ def Collect_color_from_pair_number(pair_number):
         raise Exception('Minor index out of range')
     return MAJOR_COLORS[major_index], MINOR_COLORS[minor_index]
 
+
 def get_color_index(color, color_list):
     try:
         return color_list.index(color)
     except ValueError:
         raise Exception(f'{color} index out of range')
 
-def Collect_pair_number_from_color(major_color, minor_color):
+
+def get_pair_number_from_color(major_color, minor_color):
     major_index = get_color_index(major_color, MAJOR_COLORS)
     minor_index = get_color_index(minor_color, MINOR_COLORS)
     return major_index * len(MINOR_COLORS) + minor_index + 1
